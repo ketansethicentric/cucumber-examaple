7ac4aaf73b9793b6b89306fda17c68355385584a
ketan <ketan@ketan-Inspiron-3521>
2019-04-03 04:22:28 -0400

    HUB_HOST

--- src/test/java/com/centric/steps/HomePageSteps.java
+++ src/test/java/com/centric/steps/HomePageSteps.java
@@ -9,8 +9,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 
@@ -18,11 +16,8 @@ public class HomePageSteps {
 
 	public WebDriver driver;
 
-	@Before
-	public void setup() {
-		/*System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver.exe");
-		driver = new ChromeDriver();*/
-		
+	@Given("^user is on Google Page$")
+	public void user_is_on_Google_Page() {
 		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
 		ChromeOptions options = new ChromeOptions();
 		options.merge(capabilities);
@@ -37,19 +32,6 @@ public class HomePageSteps {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
-
-	}
-
-	@After
-	public void tearDown() {
-		driver.quit();
-
-	}
-
-	@Given("^user is on Google Page$")
-	public void user_is_on_Google_Page() {
 		driver.get("https://www.google.com");
 	}
 
@@ -62,6 +44,8 @@ public class HomePageSteps {
 
 		driver.findElement(By.name("q")).sendKeys(companyName);
 		driver.findElement(By.name("btnK")).click();
+
+		driver.quit();
 	}
 
 }
